52583dc8b1248458941b2e7ad891c2dcd52fe2f3
victorlustoza <victorlustoza.qa@gmail.com>
2024-06-23 10:31:00 -0300

    Adding cy.exec with clone http

--- cypress/support/cli_commands.js
+++ cypress/support/cli_commands.js
@@ -1,5 +1,3 @@
 Cypress.Commands.add('cloneViaSSH', project => {
-  const domain = Cypress.config('baseUrl').replace('http://', '')
-
-  cy.exec(`cd cypress/downloads/ && git clone git@${domain}:${Cypress.env('user_name')}/${project.name}.git`)
+  cy.exec(`cd cypress/downloads/ && git clone ${Cypress.config('baseUrl')}/${Cypress.env('user_name')}/${project.name}.git`)
 })
